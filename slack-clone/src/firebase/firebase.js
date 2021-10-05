77339148f5153866c0faa14b6027b820efef3fe3
baso <bartlomiej.sopecki@mobica.com>
2021-10-05 15:55:59 +0200

    add firebase and semantic ui to the project

---
+++ slack-clone/src/firebase/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyCdVQWTtePcS9EDkpS5_sBrBhGBWhh90z0',
+	authDomain: 'react-slack-clone-e0e60.firebaseapp.com',
+	projectId: 'react-slack-clone-e0e60',
+	storageBucket: 'react-slack-clone-e0e60.appspot.com',
+	messagingSenderId: '83837719707',
+	appId: '1:83837719707:web:bcd20d076729324bf4a23e',
+	measurementId: 'G-6CR83KMJDT',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- slack-clone/src/index.js
+++ slack-clone/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => (
 	<Router>
 		<Switch>
-			<Route path='/' component={App} />
+			<Route exact path='/' component={App} />
+			<Route path='/login' component={Login} />
+			<Route path='/register' component={Register} />
 		</Switch>
 	</Router>
 );

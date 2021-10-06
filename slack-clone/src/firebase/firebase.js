896a7df9cddd9533236e2fd2a8f5904b5084164f
baso <bartlomiej.sopecki@mobica.com>
2021-10-06 16:25:28 +0200

    add register form

--- slack-clone/src/firebase/firebase.js
+++ slack-clone/src/firebase/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
@@ -21,6 +20,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export default app;
+export default firebase;
